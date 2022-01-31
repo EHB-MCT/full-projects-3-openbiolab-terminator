6464e3b5d97b7d333666b73dfbcf2b4723570697
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2022-01-31 15:32:34 +0100

    Replaced the homepage to second page

---
+++ app/src/main/java/com/example/open_biolab_terminator/Homepage.kt
@@ -0,0 +1,45 @@
+package com.example.open_biolab_terminator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+
+class Homepage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_homepage)
+
+        /* Goes to steps */
+        val btnGetStarted = findViewById<Button>(R.id.camerahome)
+        btnGetStarted.setOnClickListener {
+            val intent = Intent(this,Steps::class.java)
+            startActivity(intent)
+        }
+
+        /* Goes to Profile */
+        val btnAccount = findViewById<Button>(R.id.profile)
+        btnAccount.setOnClickListener {
+
+        }
+
+        /* Goes to Saved Results */
+        val btnSavedResults = findViewById<Button>(R.id.boomark)
+        btnSavedResults.setOnClickListener {
+            val intent = Intent(this,Data::class.java)
+            startActivity(intent)
+        }
+
+        /* Goes to About */
+        val btnMoreInfo = findViewById<Button>(R.id.info)
+        btnMoreInfo.setOnClickListener {
+            val intent = Intent(this,About::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
