1bc5f89d75629baedd7f8a61cb415058fc79c5a1
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2022-01-31 16:49:09 +0100

    Log in with Google

--- app/src/main/java/com/example/open_biolab_terminator/MainActivity.kt
+++ app/src/main/java/com/example/open_biolab_terminator/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.open_biolab_terminator
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
-import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+const val RC_SIGN_IN = 0
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,32 +22,31 @@ class MainActivity : AppCompatActivity() {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main)
 
-        /* Goes to steps */
-        val btnGetStarted = findViewById<Button>(R.id.camerahome)
-        btnGetStarted.setOnClickListener {
-            val intent = Intent(this,Steps::class.java)
+        val later = findViewById<TextView>(R.id.later)
+        later.setOnClickListener {
+            val intent = Intent(this,Homepage::class.java)
             startActivity(intent)
         }
 
-        /* Goes to Profile */
-        val btnAccount = findViewById<Button>(R.id.profile)
-        btnAccount.setOnClickListener {
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
 
-        }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
 
-        /* Goes to Saved Results */
-        val btnSavedResults = findViewById<Button>(R.id.boomark)
-        btnSavedResults.setOnClickListener {
-            val intent = Intent(this,Data::class.java)
-            startActivity(intent)
-        }
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        /* Goes to About */
-        val btnMoreInfo = findViewById<Button>(R.id.info)
-        btnMoreInfo.setOnClickListener {
-            val intent = Intent(this,About::class.java)
-            startActivity(intent)
+        sign_in_button.setSize(SignInButton.SIZE_STANDARD)
+
+        sign_in_button.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+
+
         }
         
     }
+
 }
