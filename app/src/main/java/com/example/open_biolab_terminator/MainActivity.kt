fc8344b363c6a511cb98ff8e1c55cad4bccb4015
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2022-02-01 09:37:26 +0100

    Completed Google sign in

--- app/src/main/java/com/example/open_biolab_terminator/MainActivity.kt
+++ app/src/main/java/com/example/open_biolab_terminator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -26,11 +27,41 @@ class MainActivity : AppCompatActivity() {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main)
 
+
+       // create all variables
         val later = findViewById<TextView>(R.id.later)
+        val btnMoreInfo = findViewById<Button>(R.id.info)
+        val btnGetStarted = findViewById<Button>(R.id.camerahome)
+        val btnAccount = findViewById<Button>(R.id.profile)
+        val btnSavedResults = findViewById<Button>(R.id.boomark)
+        val btnHomePage = findViewById<Button>(R.id.home)
+        val userName = findViewById<TextView>(R.id.welcome_user)
+
+
+        // set all visibilities
+        later.visibility = View.VISIBLE
+        userName.visibility = View.GONE
+        sign_in_button.visibility = View.VISIBLE
+        btnAccount.visibility = View.GONE
+        btnGetStarted.visibility = View.GONE
+        btnSavedResults.visibility = View.GONE
+        btnHomePage.visibility = View.GONE
+        btnMoreInfo.visibility = View.GONE
+
+
+        // Hide sign in when clicking on later
         later.setOnClickListener {
-            val intent = Intent(this,Homepage::class.java)
-            startActivity(intent)
+            later.visibility = View.GONE
+            userName.visibility = View.GONE
+            sign_in_button.visibility = View.GONE
+            btnAccount.visibility = View.VISIBLE
+            btnGetStarted.visibility = View.VISIBLE
+            btnSavedResults.visibility = View.VISIBLE
+            btnHomePage.visibility = View.VISIBLE
+            btnMoreInfo.visibility = View.VISIBLE
+
         }
+
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -53,7 +84,40 @@ class MainActivity : AppCompatActivity() {
         //Check if the user is already signed in
         val acct = GoogleSignIn.getLastSignedInAccount(this)
         if (acct != null) {
-            val intent = Intent(this,Homepage::class.java)
+            later.visibility = View.GONE
+            sign_in_button.visibility = View.GONE
+            btnAccount.visibility = View.VISIBLE
+            btnGetStarted.visibility = View.VISIBLE
+            btnSavedResults.visibility = View.VISIBLE
+            btnHomePage.visibility = View.VISIBLE
+            userName.text = "Welcome " + acct.displayName
+            userName.visibility = View.VISIBLE
+            btnMoreInfo.visibility = View.VISIBLE
+        }
+
+
+
+        /* Goes to steps */
+        btnGetStarted.setOnClickListener {
+            val intent = Intent(this,Steps::class.java)
+            startActivity(intent)
+        }
+
+        /* Goes to Profile */
+        btnAccount.setOnClickListener {
+            val intent = Intent(this,Profile::class.java)
+            startActivity(intent)
+        }
+
+        /* Goes to Saved Results */
+        btnSavedResults.setOnClickListener {
+            val intent = Intent(this,Data::class.java)
+            startActivity(intent)
+        }
+
+        /* Goes to About */
+        btnMoreInfo.setOnClickListener {
+            val intent = Intent(this,About::class.java)
             startActivity(intent)
         }
     }
@@ -72,17 +136,35 @@ class MainActivity : AppCompatActivity() {
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
 
-        try {
-            completedTask.getResult(ApiException::class.java)
+        val later = findViewById<TextView>(R.id.later)
+        val btnMoreInfo = findViewById<Button>(R.id.info)
+        val btnGetStarted = findViewById<Button>(R.id.camerahome)
+        val btnAccount = findViewById<Button>(R.id.profile)
+        val btnSavedResults = findViewById<Button>(R.id.boomark)
+        val btnHomePage = findViewById<Button>(R.id.home)
+        val userName = findViewById<TextView>(R.id.welcome_user)
 
-            val intent = Intent(this,Homepage::class.java)
-            startActivity(intent)
+
+        try {
+           val account = completedTask.getResult(ApiException::class.java)
+
+            later.visibility = View.GONE
+            sign_in_button.visibility = View.GONE
+            btnAccount.visibility = View.VISIBLE
+            btnGetStarted.visibility = View.VISIBLE
+            btnSavedResults.visibility = View.VISIBLE
+            btnHomePage.visibility = View.VISIBLE
+            userName.text = "Welcome " + account.displayName
+            userName.visibility = View.VISIBLE
+            btnMoreInfo.visibility = View.VISIBLE
+
+
 
         } catch (e: ApiException) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
 
-            Toast.makeText(baseContext, "Unvalid data", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Unvalid data", Toast.LENGTH_SHORT).show()
 
         }
     }
