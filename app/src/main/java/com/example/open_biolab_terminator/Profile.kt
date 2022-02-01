acfb19aed8a5aa41b1cdadd7e9b87f1f62de2292
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2022-02-01 10:35:46 +0100

    Added Google sign in to profile page

--- app/src/main/java/com/example/open_biolab_terminator/Profile.kt
+++ app/src/main/java/com/example/open_biolab_terminator/Profile.kt
@@ -3,16 +3,69 @@ package com.example.open_biolab_terminator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_main.*
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_datapage)
+        setContentView(R.layout.activity_profile)
+
+
+        /* Log in to show details */
+        val name = findViewById<TextView>(R.id.name)
+        val email = findViewById<TextView>(R.id.email)
+        val loginText = findViewById<TextView>(R.id.login)
+
+        name.visibility = View.GONE
+        email.visibility = View.GONE
+        sign_in_button.visibility = View.VISIBLE
+        loginText.visibility = View.VISIBLE
+
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        sign_in_button.setSize(SignInButton.SIZE_STANDARD)
+
+        sign_in_button.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+
+
+        }
+        //Check if the user is already signed in
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        if (acct != null) {
+            name.text = acct.displayName
+            name.visibility = View.VISIBLE
+            email.text = acct.email
+            email.visibility = View.VISIBLE
+            sign_in_button.visibility = View.GONE
+            loginText.visibility = View.GONE
+
+        }
+
+
+
 
         /* Home Button */
         /* Gets button id */
@@ -45,5 +98,47 @@ class Profile : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+
+        val name = findViewById<TextView>(R.id.name)
+        val email = findViewById<TextView>(R.id.email)
+        val loginText = findViewById<TextView>(R.id.login)
+
+
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            name.text = account.displayName
+            name.visibility = View.VISIBLE
+            email.text = account.email
+            email.visibility = View.VISIBLE
+            sign_in_button.visibility = View.GONE
+            loginText.visibility = View.GONE
+
+
+
+
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+
+            Toast.makeText(this, "Unvalid data", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
