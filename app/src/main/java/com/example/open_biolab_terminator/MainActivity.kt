d2fc9b7c598131377899034825e5c186bc04bd65
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2022-02-02 11:01:44 +0100

    Authentication if Firebase in homepage

--- app/src/main/java/com/example/open_biolab_terminator/MainActivity.kt
+++ app/src/main/java/com/example/open_biolab_terminator/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.open_biolab_terminator
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
@@ -16,10 +18,15 @@ import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 const val RC_SIGN_IN = 0
+private lateinit var auth: FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +46,8 @@ class MainActivity : AppCompatActivity() {
         val logout = findViewById<TextView>(R.id.logout)
 
 
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
 
         // set all visibilities
@@ -57,7 +66,9 @@ class MainActivity : AppCompatActivity() {
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
@@ -104,6 +115,8 @@ class MainActivity : AppCompatActivity() {
                     logout.visibility = View.GONE
 
                 })
+
+            Firebase.auth.signOut()
         }
 
 
@@ -171,7 +184,8 @@ class MainActivity : AppCompatActivity() {
             btnMoreInfo.visibility = View.VISIBLE
             logout.visibility = View.VISIBLE
 
-
+            val id = account.idToken
+            firebaseAuthWithGoogle(id.toString())
 
 
         } catch (e: ApiException) {
@@ -182,5 +196,27 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        var currentUser = auth.getCurrentUser()
+        //updateUI(currentUser);
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    //updateUI(null)
+                }
 
 }
