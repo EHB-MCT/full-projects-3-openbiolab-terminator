11a14c221351c194ca7dda125d92f85b52f42a09
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2022-02-03 15:08:56 +0100

    Removed Firebase database imports

--- app/src/main/java/com/example/open_biolab_terminator/MainActivity.kt
+++ app/src/main/java/com/example/open_biolab_terminator/MainActivity.kt
@@ -21,13 +21,6 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
-import com.google.firebase.firestore.SetOptions
-import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
